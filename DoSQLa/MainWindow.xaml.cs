e34a6f6c9e69cd92a3082ac99cf2aee4a886f575
staddaleko <staddaleko@gmail.com>
2014-07-23 13:46:14 +0200

    1346 działa!

---
+++ DoSQLa/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoSQLa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+
+
+        //private void DataGrid_Initialized(object sender, EventArgs e)
+        //{
+        //    DataClasses1DataContext dba = new DataClasses1DataContext();
+        //    var query = (from s in dba.StudentTabelas select new { s.Imię, s.Nazwisko, s.Wiek });
+        //    Stjudenci.ItemsSource = query.ToList();
+        //}
+
+
+
+        private void ComboBox_Initialized(object sender, EventArgs e)
+        {
+            DataClasses1DataContext dba = new DataClasses1DataContext();
+            var zapytanie = (from s in dba.StudentTabelas select new { s.ID, s.Imię, s.Nazwisko });
+            ComboStjudent.ItemsSource = zapytanie.ToList();
+
+        }
+
+        private void ComboStjudent_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataClasses1DataContext db = new DataClasses1DataContext();
+            string uczen = ComboStjudent.SelectedItem.ToString();
+            string uczenPierwszyEtap = uczen.Substring(6, 2);
+            //UczenPierwszyEtap.Content = uczenPierwszyEtap;
+            string obciecie = uczen.Remove(0, 6);
+            //testLabel.Content = obciecie;
+            string searchWithinThis = obciecie;
+            string searchForThis = ",";
+            int firstCharacter = searchWithinThis.IndexOf(searchForThis);
+            //testLabel.Content = firstCharacter;
+            string obciecie2 = obciecie.Remove(firstCharacter);
+            int id = Convert.ToInt32(obciecie2);
+            var zapytanie = (from p in db.Przedmioties
+                             join o in db.Stu_Przeds on p.ID_przedmiotu equals o.ID_Przedmiotu
+                             join u in db.StudentTabelas on o.ID_Studenta equals u.ID
+                             where u.ID == id
+                             select new { p.Nazwa_przedmiotu, o.Ocena });
+            Stjudenci.ItemsSource = zapytanie.ToList();
+        }
+
+
+    }
+}
